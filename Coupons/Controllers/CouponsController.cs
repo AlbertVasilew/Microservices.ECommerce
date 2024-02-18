13c5a44b0368940d1b6fd1dc6d256f7a8314710c
Albert Vasilev <avasilev@gmail.com>
2024-02-18 19:05:20 +0200

    [Coupons] Add authentication & authorization

--- Coupons/Controllers/CouponsController.cs
+++ Coupons/Controllers/CouponsController.cs
@@ -3,6 +3,7 @@ using Coupons.Handlers.Get;
 using Coupons.Handlers.GetById;
 using Coupons.Handlers.Upsert;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Coupons.Controllers
@@ -19,20 +20,24 @@ namespace Coupons.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Get()
             => Ok(await mediator.Send(new GetRequest()));
 
         [HttpGet]
         [Route("{id}")]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> GetById(int id)
             => Ok(await mediator.Send(new GetByIdRequest { Id = id }));
 
         [HttpPost]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Upsert(UpsertRequest upsertRequest)
             => Ok(await mediator.Send(upsertRequest));
 
         [HttpDelete]
         [Route("{id}")]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Delete(int id)
             => Ok(await mediator.Send(new DeleteRequest { Id = id }));
     }
