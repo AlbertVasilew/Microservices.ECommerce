88d143fd6f8001816139d2b5ca8bfa5740b9e458
Albert Vasilev <avasilev@gmail.com>
2024-02-18 15:38:21 +0200

    [Coupons] Add EF Core 8 and create Coupon model

---
+++ Coupons/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Coupons.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coupons.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Coupons/Program.cs
+++ Coupons/Program.cs
@@ -1,9 +1,15 @@
+using Coupons.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
